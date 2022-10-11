73b8490e0ac1f279f651ef9872cbbb347d4392d3
Jabaaay <115509116+Jabaaay@users.noreply.github.com>
2022-10-11 17:33:52 +0800

    Add files via upload

---
+++ Hello_World.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+
+     int num=100;
+     float temp=50;
+
+    printf("the total value is %d\n",num);
+    printf("the total value is %f",temp);
+
+    return 0;
+
+}
